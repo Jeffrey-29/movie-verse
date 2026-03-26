18d4813fb164b280d079c52e3738fecfa3cf3691
Jeffrey Gabriel <jeffrey@Jeffreys-MacBook-Air.local>
2026-03-26 19:45:21 +0530

    update on port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const TMDB_TOKEN = process.env.TMDB_TOKEN;
 app.use(express.static("public"));
